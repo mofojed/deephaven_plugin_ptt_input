de93ef63a3d3c246784a4c2a5c6928162a91f4df
mikebender <mikebender@deephaven.io>
2024-07-31 09:30:55 -0400

    Make some corrections from the template

--- src/js/src/DeephavenPluginPttInputView.tsx
+++ src/js/src/DeephavenPluginPttInputView.tsx
@@ -1,11 +1,13 @@
-import React, { CSSProperties, useEffect, useState } from 'react';
-import { useApi } from '@deephaven/jsapi-bootstrap';
-import Log from '@deephaven/log';
-import { WidgetComponentProps } from '@deephaven/plugin';
-import type { Widget } from '@deephaven/jsapi-types';
-import { Button, TextField } from '@deephaven/components';
+import React, { CSSProperties, useEffect, useState } from "react";
+import { useApi } from "@deephaven/jsapi-bootstrap";
+import Log from "@deephaven/log";
+import { WidgetComponentProps } from "@deephaven/plugin";
+import type { dh } from "@deephaven/jsapi-types";
+import { Button, TextField } from "@deephaven/components";
 
-const log = Log.module('deephaven-plugin-ptt-input.DeephavenPluginPttInputView');
+const log = Log.module(
+  "deephaven-plugin-ptt-input.DeephavenPluginPttInputView"
+);
 
 // Create a custom style for the component
 export const DeephavenPluginPttInputViewStyle: CSSProperties = {
@@ -17,33 +19,36 @@ export const DeephavenPluginPttInputViewStyle: CSSProperties = {
   alignItems: "center",
   height: "100%",
   width: "100%",
-  flexDirection: "column"
+  flexDirection: "column",
 };
 
-export function DeephavenPluginPttInputView(props: WidgetComponentProps): JSX.Element {
+export function DeephavenPluginPttInputView(
+  props: WidgetComponentProps
+): JSX.Element {
   const { fetch } = props;
-  const [text, setText] = useState<string>("Call send_message on the object and the message will appear here.");
-  const [formText, setFormText] = useState('');
-  const [widget, setWidget] = useState<Widget | null>(null);
+  const [text, setText] = useState<string>(
+    "Call send_message on the object and the message will appear here."
+  );
+  const [formText, setFormText] = useState("");
+  const [widget, setWidget] = useState<dh.Widget | null>(null);
   const dh = useApi();
 
   useEffect(() => {
     async function init() {
-       // Fetch the widget from the server
-      const fetched_widget = await fetch() as Widget;
+      // Fetch the widget from the server
+      const fetched_widget = (await fetch()) as dh.Widget;
       setWidget(fetched_widget);
 
-
       // Add an event listener to the widget to listen for messages from the server
-      fetched_widget.addEventListener<Widget>(
-          dh.Widget.EVENT_MESSAGE,
-          ({ detail }) => {
-            // When a message is received, update the text in the component
-            const text = detail.getDataAsString();
-            if (text) {
-              setText(text);
-            }
+      fetched_widget.addEventListener<dh.Widget>(
+        dh.Widget.EVENT_MESSAGE,
+        ({ detail }) => {
+          // When a message is received, update the text in the component
+          const text = detail.getDataAsString();
+          if (text) {
+            setText(text);
           }
+        }
       );
     }
 
@@ -55,9 +60,9 @@ export function DeephavenPluginPttInputView(props: WidgetComponentProps): JSX.El
       <div>{text}</div>
       <div>Send a message to the server:</div>
       <TextField
-          value={formText}
-          onChange={(value) => setFormText(value)}
-          marginBottom='size-50'
+        value={formText}
+        onChange={(value) => setFormText(value)}
+        marginBottom="size-50"
       />
       <Button
         onClick={() => {
@@ -65,7 +70,9 @@ export function DeephavenPluginPttInputView(props: WidgetComponentProps): JSX.El
           if (widget) {
             widget.sendMessage(formText, []);
           }
-        }}>
+        }}
+        kind="primary"
+      >
         Send
       </Button>
     </div>
